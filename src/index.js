01fa899f9821770c3466fb8d4c186bac2d4d05eb
IharTsykaka <35345292+IharTsykaka@users.noreply.github.com>
2018-03-04 09:07:55 +0300

    Task 08

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    var zeroCount = number;
+    for(var i = 2; i <= base; i++){
+        if(base % i == 0){
+            var baseMax = 0;
+            while(base % i == 0) { 
+                baseMax++;
+                base /= i;
+            }
+            var numCopy = number;
+            var numCount = 0;
+            while (numCopy / i > 0) {
+                numCount += Math.floor(numCopy / i);		  
+                numCopy /= i;	
+            }
+            if(zeroCount > numCount / baseMax)
+                zeroCount = numCount / baseMax;
+        }
+    }
+    return Math.floor(zeroCount);  
 }
